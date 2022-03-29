36544e409e41a9bcef6bfefe43752bc5f72fdc76
Couturier alexandre <couturier@MacBook-Pro-de-Couturier.local>
2022-03-29 16:56:14 +0200

    Header php

---
+++ headerFooter.php
@@ -0,0 +1,41 @@
+<?php
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <header>
+        <ul>
+            <li> <a href="">Accueil</a> </li>
+            <li><a href="">À propos</a></li>
+            <li> <img src="./pictures/logo.png" alt=""></li>
+            <li><a href="">Parcourir les offres</a></li>
+            <li><a href="">Publier une offre</a></li>
+        </ul>
+    </header>
+
+    <footer>
+        <ul>
+            <li><a href=""></a></li>
+            <li><a href=""></a></li>
+            <li><a href=""></a></li>
+            <li><a href=""></a></li>
+            <li><a href=""></a></li>
+        </ul>
+        <ul>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+        </ul>
+    </footer>
+</body>
+</html>
