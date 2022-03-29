f7ebb8a4352d01dce3b780f679c2f6bdd3ff7b6a
wallshck <Mybnkoua@gmail.com>
2022-03-29 14:58:52 +0200

    debut css

--- View/Html/index.php
+++ View/Html/index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -7,41 +8,79 @@
     <link rel="stylesheet" href="../Css/style.css">
     <title>Document</title>
 </head>
+
 <body>
+    <!-- Haut de page-->
     <header class="">
         <div class="logo">
             <img src="../Pictures/logo.png" alt="" srcset="">
         </div>
-    <nav>
-        <ul>
-            <li>accueil</li>
-            <li>à propos</li>
-            <li>parcourir les offres</li>
-            <li>publier une offre</li>
-        </ul>
-    </nav>
+        <nav>
+            <ul>
+                <li>accueil</li>
+                <li>à propos</li>
+                <li>parcourir les offres</li>
+                <li>publier une offre</li>
+            </ul>
+        </nav>
         <div class="login">
             <div>inscription</div>
             <div>connexion</div>
         </div>
     </header>
 
+    <!-- Header avec l'image slide-->
     <div class="headimg">
-
+        <h1>L’expert emplois 100 % Sport</h1>
     </div>
 
+
+    <!-- premiere section-->
     <div class="section first">
         <div>
-                <img src="" alt="">
+            <img src="../Pictures/1.png" alt="">
         </div>
         <div>
-            <h3></h3>
+            <h3>NOUS CONSEILLONS</h3>
         </div>
-        
+        <div>
+            <img src="../Pictures/11.png" alt="">
+        </div>
+        <div>
+            <p>dans la définition du profil idéal, la mise en place d’une stratégie de communication RH adéquate, la constitution d’un organigramme ou encore sur les pratiques économiques du marché</p>
+        </div>
     </div>
-    <div class="section second"></div>
-    <div class="section third"></div>
 
+    <!-- deuxieme section-->
+    <div class="section second">
+        <div>
+            <img src="../Pictures/2.png" alt="">
+        </div>
+        <div>
+            <h3>NOUS ACCOMPAGNONS</h3>
+        </div>
+        <div>
+            <img src="../Pictures/22.png" alt="">
+        </div>
+        <div>
+            <p>dans la recherche des meilleurs candidats par notre connaissance des acteurs, le bouche à oreille, des interventions auprès de colloques et formations de renoms...</p>
+        </div>
+    </div>
+
+
+    <!-- troisieme section-->
+    <div class="section third">
+        <div>
+            <h3>NOUS VALORISONS</h3>
+        </div>
+        <div>
+            <p>Sjob.com regroupant sous la forme d’un job-board les offres d’emploi de nos clients, et proposant des outils pour favoriser leur attractivité.</p>
+        </div>
+
+    </div>
+
+
+    <!-- bas de page-->
     <footer class="">
 
         <div class="logofooter">
@@ -50,10 +89,11 @@
 
         <div class="footernav"></div>
         <div class="socialMedia"></div>
-        
+        <p>Copyright © 2020 Sjob’ group</p>
     </footer>
 
 
     <script src="../Js/javascritp.js"></script>
 </body>
+
 </html>
