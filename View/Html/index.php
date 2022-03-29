2bbc68350f087f813a00cfc4847ce238d86a960e
wallshck <Mybnkoua@gmail.com>
2022-03-29 14:06:14 +0200

    code

---
+++ View/Html/index.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../Css/style.css">
+    <title>Document</title>
+</head>
+<body>
+    <header class="">
+        <div class="logo">
+            <img src="../Pictures/logo.png" alt="" srcset="">
+        </div>
+    <nav>
+        <ul>
+            <li>accueil</li>
+            <li>à propos</li>
+            <li>parcourir les offres</li>
+            <li>publier une offre</li>
+        </ul>
+    </nav>
+        <div class="login">
+            <div>inscription</div>
+            <div>connexion</div>
+        </div>
+    </header>
+
+    <div class="headimg">
+
+    </div>
+
+    <div class="section first">
+        <div>
+                <img src="" alt="">
+        </div>
+        <div>
+            <h3></h3>
+        </div>
+        
+    </div>
+    <div class="section second"></div>
+    <div class="section third"></div>
+
+    <footer class="">
+
+        <div class="logofooter">
+            <img src="../Pictures/logo.png" alt="" srcset="">
+        </div>
+
+        <div class="footernav"></div>
+        <div class="socialMedia"></div>
+        
+    </footer>
+
+
+    <script src="../Js/javascritp.js"></script>
+</body>
+</html>
