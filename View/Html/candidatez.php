d4a9bc6b3af7923323ff80762728466007c77057
wallshck <Mybnkoua@gmail.com>
2022-03-30 14:05:01 +0200

    modif du css formulaire

--- View/Html/candidatez.php
+++ View/Html/candidatez.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="../Css/candidatez.css">
     <title>Document</title>
 </head>
 <body>
@@ -13,23 +13,26 @@
         ?>
 
     <div class="form">
-        <h1>CANDIDATEZ</h1>
-        <form action="" method="">
-            <label for="">Nom</label>
-            <input type="text" id="" name="Nom" value=""><br>
+        <div class="candid">
+            <h1>CANDIDATEZ</h1>
+            <form action="" method="">
+                <label for="">Nom</label><br>
+                <input type="text" id="" name="Nom" value="" placeholder="Exemple : Dupont" required><br><br>
 
-            <label for="">Nom</label>
-            <input type="text" id="" name="Nom" value=""><br>
+                <label for="">Prenom</label><br>
+                <input type="text" id="" name="Nom" value="" placeholder="Exemple : Alain" required><br><br>
+            <div class="inli">
+                 <div>
+                        <label for="">Email</label>
+                        <input type="mail" id="" name="email" value=""  required>
 
-            <label for="">Nom</label>
-            <input type="text" id="" name="Nom" value="">
-            <label for="">Nom</label>
-            <input type="text" id="" name="Nom" value=""><br><br>
-
-            <label for="">Nom</label>
-            <input type="text" id="" name="Nom" value="">
-
-
+                        <label for="">Telephone</label>
+                        <input type="int" id="" name="telephone" value=""  required><br><br>
+                </div>
+            </div>
+                <label for="">Description</label><br>
+                <textarea name="description" rows="8" cols="60" ></textarea>        
+        </div>
 
 
 
