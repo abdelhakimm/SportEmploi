eae4b4a3253dc6024424c3d758713885986331de
TTHELLIER <t.thellier@gmail.com>
2022-03-30 10:05:59 +0200

    retravail sur wireframe + page d'accueil et maquettage figma

--- View/viewJob.php/formUpdateJob.php
+++ View/viewJob.php/formUpdateJob.php
@@ -10,6 +10,31 @@
 <body>
 <a href="../../Back/Router/RouterJob.php?action=all">Les Annonces</a>
 
+    <form action="../../Back/Router/RouterJob.php" method="post">
 
+        <input type="hidden" name="id" value="<?=$Job['Id_Job']?>">
+
+        <label for = "firstname"> Prénom </label>
+        <input type = "text" name = "firstname" id = "firstname" required value="<?=$Job['firstname']?>"> <br>
+
+        <label for = "lastname"> Nom </label> 
+        <input type = "text" name = "lastname" id = "lastname" value="<?=$Job['lastname']?>" required> <br>
+
+        <label for = "mail"> Email </label> 
+        <input type = "text" name = "mail" id = "mail" value="<?=$Job['mail']?>" required> <br>
+
+        <label for = "phone"> Téléphone </label>
+        <input type = "number" name = "phone" id = "phone" value="<?=$Job['phone']?>"> <br>
+
+        <label for = "motivation"> Lettre de motivation </label>
+        <input type = "number" name = "motivation" id = "motivation" value="<?=$Job['motivation']?>"> <br>
+
+        <label for = "name_job"> Nom de l'annonce </label>
+        <input type = "number" name = "name_job" id = "name_job" value="<?=$Job['name_job']?>"> <br>
+
+        <label for="Envoyer"></label>
+        <input type="submit" name="submit"  value="Envoyer">
+
+    </form>
 </body>
 </html>
