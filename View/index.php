437ef537b48ac78c273df65b5485edf86792125c
Couturier alexandre <couturier@MacBook-Pro-de-Couturier.local>
2022-03-30 14:27:21 +0200

    Ajout de footer dans la page accueil

--- View/index.php
+++ View/index.php
@@ -39,19 +39,19 @@
         </div>
         <div id="descriptionAccueil">
         <div class="paragrapheAccueil">
-            <p class="titreParagraphe">NOUS CONSEILLONS</p>
-            <p>Dans la définition du profil idéal, la mise en place d’une stratégie de communication RH adéquate, la constitution d’un organigramme ou encore sur les pratiques économiques du marché.</p>
+            <p class="titreParagraphe" id="couleurUn">NOUS CONSEILLONS</p>
+            <p class="descriptionParagraphe">Dans la définition du profil idéal, la mise en place d’une stratégie de communication RH adéquate, la constitution d’un organigramme ou encore sur les pratiques économiques du marché.</p>
         </div>
         <div class="paragrapheAccueil">
             <p class="titreParagraphe">NOUS ACCOMPAGNONS</p>
-            <p>Dans la recherche des meilleurs candidats par notre connaissance des acteurs, le bouche à oreille, des interventions auprès de colloques et formations de renoms...</p>
+            <p class="descriptionParagraphe">Dans la recherche des meilleurs candidats par notre connaissance des acteurs, le bouche à oreille, des interventions auprès de colloques et formations de renoms...</p>
         </div>
         <div class="paragrapheAccueil">
-            <p class="titreParagraphe">NOUS VALORISONS</p>
-            <p>Sjob.com regroupant sous la forme d’un job-board les offres d’emploi de nos clients, et proposant des outils pour favoriser leur attractivité.</p>
+            <p class="titreParagraphe" id="couleurTrois">NOUS VALORISONS</p>
+            <p class="descriptionParagraphe">Sjob.com regroupant sous la forme d’un job-board les offres d’emploi de nos clients, et proposant des outils pour favoriser leur attractivité.</p>
         </div>
         </div>
     </section>
-    
+    <?php require_once "footer.php" ?>
 </body>
 </html>
