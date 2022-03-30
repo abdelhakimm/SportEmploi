2ce7ed1ced5a5804aec9dafee1986ac3a85e7620
Couturier alexandre <couturier@MacBook-Pro-de-Couturier.local>
2022-03-30 22:03:04 +0200

    Page offrePost finish

---
+++ View/offrePost.php
@@ -0,0 +1,42 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="./css/style.css">
+    <link rel="stylesheet" href="./fontawesome/css/all.css">
+</head>
+<body>
+    <?php require_once "headerFooter.php" ?>
+    <section>
+        <div class="textHaut">
+            <p class="paragrapheHaut">TOUTE LES OFFRES QUE NOUS PROPOSONS SONT ICI : </p>
+        </div>
+        <div id="sectionPost">
+            <div class="postAdd">
+                <p class="postAddUn">Nom du poste </p>
+                <p class="postAddDeux">Type de contrat - Nom de l’entreprise - Ville</p>
+                <p class="postAddTrois">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic.</p>
+                <div class="cadrePost">
+                    <p class="cadrePostText">Postuler</p>
+                </div>
+            </div>
+            <div id="sectionPost">
+            <div class="postAdd">
+                <p class="postAddUn">Nom du poste </p>
+                <p class="postAddDeux">Type de contrat - Nom de l’entreprise - Ville</p>
+                <p class="postAddTrois">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic.</p>
+                <div class="cadrePost">
+                    <p class="cadrePostText">Postuler</p>
+                </div>
+            </div>
+        </div>
+    </section>
+    <?php require_once "footer.php" ?>
+</body>
+</html>
