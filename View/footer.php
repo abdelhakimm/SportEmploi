b6b6305b27aea4ef6e7760b714fd88b50f57afdd
Couturier alexandre <couturier@MacBook-Pro-de-Couturier.local>
2022-03-30 12:00:04 +0200

    Ajout image Accueil

---
+++ View/footer.php
@@ -0,0 +1,28 @@
+<?php
+
+
+?>
+
+
+<footer>
+        <ul>
+            <li class="menu" id="menuSolo"><a href="">Menu :</a></li>
+            <li><a href="">Accueil</a></li>
+            <li><a href="">À propos</a></li>
+            <li><a href="">Parcourir les offres</a></li>
+            <li><a href="">Publier une offre</a></li>
+        </ul>
+        <ul>
+            <li class="menu">Réseaux sociaux :</li>
+            <li class="iconeSociaux"><i class="fa-brands fa-facebook"></i></li>
+            <li class="iconeSociaux"><i class="fa-brands fa-linkedin-in"></i></li>
+            <li class="iconeSociaux"><i class="fa-brands fa-twitter"></i></li>
+        </ul>
+        <div>
+            <img src="./pictures/logo.png" alt="">
+            <div id="copyright">
+            <i class="fa-regular fa-copyright"></i>
+            <p>S’JOB, 2022</p>
+            </div>
+
+        </div>
---
+++ View/index.php
@@ -0,0 +1,22 @@
+<?php
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="./css/style.css">
+    <link rel="stylesheet" href="./fontawesome/css/all.css">
+</head>
+<body>
+<?php require_once "headerFooter.php" ?>
+    <section>
+        <div id="backgroundAccueil">
+            <img src="./pictures/background.jpg" alt="">
+        </div>
+    </section>
+    
+</body>
+</html>
