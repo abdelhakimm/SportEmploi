96dcc0b162ae463e44c625f3211b437b33e2d3d5
wallshck <Mybnkoua@gmail.com>
2022-03-30 16:31:32 +0200

    again new file parcourir html and css

--- View/Html/PublierUneOffre.php
+++ View/Html/PublierUneOffre.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -7,15 +8,16 @@
     <link rel="stylesheet" href="../Css/publier.css">
     <title>Document</title>
 </head>
+
 <body>
 
-<?php
+    <?php
 
-require "header.php";
+    require "header.php";
 
-?>
+    ?>
 
-<div class="form">
+    <div class="form">
         <div class="candid">
             <h1>Publier une offre</h1>
             <form action="" method="">
@@ -38,31 +40,33 @@ require "header.php";
                 <input type="text" id="" name="Nom" value="" placeholder="Exemple" required><br><br>
 
 
-            <div class="inli">
-                 <div>
+                <div class="inli">
+                    <div>
                         <label for="">date de début</label><br>
                         <input type="mail" id="" name="email" value="" placeholder="ExempleAlain@gmail.com" required>
 
                         <label for="">date de fin</label>
                         <input type="number" id="" name="telephone" value="" placeholder="Exemple : 07********" required><br><br>
+                    </div>
                 </div>
-            </div>
 
-                 <label for="">Localisation</label><br>
+                <label for="">Localisation</label><br>
                 <input type="text" id="" name="Nom" value="" placeholder="Exemple" required><br><br>
 
                 <label for="">Description</label><br>
-                <textarea name="description" rows="8" cols="50" ></textarea>
+                <textarea name="description" rows="8" cols="90"></textarea>
 
                 <div class="sub">
-                <input type="submit">
+                    <input type="submit" value="Envoyé">
                 </div>
-                
-        </div>
+            </form>
 
-        <?php 
-        require "footer.php";
-        ?>
+        </div>
+    </div>
+    <?php
+    require "footer.php";
+    ?>
 
 </body>
+
 </html>
