412a7d5fada51a20e3fbec702f63b147fafe54a1
Couturier alexandre <couturier@MacBook-Pro-de-Couturier.local>
2022-03-30 15:32:51 +0200

    modification de la section presention Alain

--- View/apropos.php
+++ View/apropos.php
@@ -14,13 +14,21 @@
 <body>
     <?php require_once "headerFooter.php" ?>
     <section>
-        <div>
-            <p></p>
-            <p></p>
-            <img src="" alt="">
-        </div>
-        <div>
-
+        <div id="sectionAccueilUn">
+            <div id="descriptionAlain">
+                <p id="titrePropos">Bienvenue sur <span>S’Job</span>, dans « votre » univers : le Sport et l’Entertainment !</p>
+                <p>
+                Premier Sjob, Cabinet de recrutement et de conseil à s’être créé en Europe en 2001, nous sommes très fiers d’être les précurseurs du marché et de représenter l’expertise française. <br><br>
+                Acteurs nationaux ou internationaux, acteurs de proximité au cœur des problématiques sportives, Sjob vous propose un service global ressources humaines pour répondre à vos besoins. Tout un monde, un univers autour d’une passion commune : le sport. <br><br>
+                Chez Sjob, notre ambition est simple : offrir la meilleure expérience de recrutement possible pour les candidats et les entreprises du marché du sport et de l’Entertainment. <br><br>
+                 Depuis 2001, nous développons des outils évolutifs adaptés aux spécificités et évolutions du secteur à travers notre plateforme Sjob.com et notre cabinet de recrutement expert du Sport et de l’Entertainment. <br><br>
+                Merci de votre fidélité, merci aux nouveaux venus dans notre communauté d’experts. Suivez-nous, ce n’est pas fini... <br><br>
+                </p>
+                <img src="./pictures/signature.png" alt="">
+            </div>
+            <div>
+                <img src="./pictures/Alainghibaudo.png" alt="">
+            </div>
         </div>
     </section>
 </body>
