e973c6c4789b103980f68a96c6911eae15dce258
Couturier alexandre <couturier@MacBook-Pro-de-Couturier.local>
2022-03-30 15:14:08 +0200

    création section de la page A propos

---
+++ View/apropos.php
@@ -0,0 +1,27 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="./css/style.css">
+    <link rel="stylesheet" href="./fontawesome/css/all.css">
+</head>
+<body>
+    <?php require_once "headerFooter.php" ?>
+    <section>
+        <div>
+            <p></p>
+            <p></p>
+            <img src="" alt="">
+        </div>
+        <div>
+
+        </div>
+    </section>
+</body>
+</html>
