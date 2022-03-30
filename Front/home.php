4e51e7de8c87a3af239c8b930dee08d4a50d0c45
Alain <alainczr22@gmail.com>
2022-03-30 23:21:43 +0200

    hmtl and css of home.php

--- Front/home.php
+++ Front/home.php
@@ -5,31 +5,35 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home</title>
+    <link rel="stylesheet" href="./Style/home.css">
+    <link rel="stylesheet" href="./font/fontawesome-free-6.0.0-web/css/all.css">
 </head>
 <body>
-<header>
-            <div class="logo">
-                <img src="./logoEtIcon/logo.png" width="100px" height="100px">
-            </div>
 
-            <nav>
-                <ul>
-                    <div id="allNav">
-                        <div class="flexNav">
-                           <li><a href="https://developer.mozilla.org/en-US/docs/Web/CSS/Specificity">Accueil</a></li>
-                           <li><a href="https://developer.mozilla.org/en-US/docs/Web/CSS/Specificity">A propos</a></li>
-                        </div>
-                        <div class="flexNav">
-                           <li><a href="https://developer.mozilla.org/en-US/docs/Web/CSS/Specificity">Parcourir les offres</a></li>
-                           <li><a href="https://developer.mozilla.org/en-US/docs/Web/CSS/Specificity">Publier une offre</a></li>
-                        </div>
-                    </div>
-                </ul>
-            </nav>
-    </header>
+    
+    <div id="fond">
+        <header>  
+                <div class="logo">
+                    <img src="./logoEtIcon/logo.png" width="100px" height="100px">
+                </div>
 
-    <div>
-        <img src="./Image/SliderHomev2.jpg" >
+                <nav>
+                    <ul>
+                        <div id="allNav">
+                            <div class="flexNav">
+                               <li><a href="https://developer.mozilla.org/en-US/docs/Web/CSS/Specificity">Accueil</a></li>
+                               <li><a href="https://developer.mozilla.org/en-US/docs/Web/CSS/Specificity">A propos</a></li>
+                            </div>
+                            <div class="flexNav">
+                               <li><a href="https://developer.mozilla.org/en-US/docs/Web/CSS/Specificity">Parcourir les offres</a></li>
+                               <li><a href="https://developer.mozilla.org/en-US/docs/Web/CSS/Specificity">Publier une offre</a></li>
+                            </div>
+                        </div>
+                    </ul>
+                </nav>
+        </header>
+
+    
 
         <h1>L’EXPERT EMPLOI 100% SPORT</h1>
     </div>  
@@ -37,36 +41,74 @@
     <h2>POURQUOI CHOISIR S’JOB ?</h2>  
 
     <div>
-        <div>
-            <img src="./logoEtIcon/crosshair-100x100.png" >
-            <img src="./logoEtIcon/computer-100x100.png" >
-            <img src="./logoEtIcon/network-100x100.png" >
-            <img src="./logoEtIcon/project-management-100x100.png" >
-        </div>
-        <div>
-            <h4>NOUS CONSEILLONS</h4>
-            <p>
-                Dans la définition du profil idéal, la mise en place d’une stratégie de
+        <section>
+            <div>
+                <img src="./logoEtIcon/crosshair-100x100.png">  
+                <p>CABINET DE RECRUTEMENT</p>
+            </div>
+            <div>
+                <img src= "./logoEtIcon/computer-100x100.png">
+                <p>JOBOARD</p>
+            </div>
+            <div>
+                <img src= "./logoEtIcon/network-100x100.png">
+                <p>MEDIA</p>
+            </div>
+            <div>
+                <img src="./logoEtIcon/project-management-100x100.png" >
+                <p>ACCOMPAGNEMENT ET CONSEIL</p>
+            </div>
+        </section>
+
+
+        <section>
+            <div>
+                <h4>NOUS CONSEILLONS</h4>
+                <p>Dans la définition du profil idéal, la mise en place d’une stratégie de
                 communication RH adéquate, la constitution d’un organigramme ou encore sur
-                les pratiques économiques du marché
-            </p>
-        </div>
-        <div>
-            <h4>NOUS ACCOMPAGNONS</h4>
-            <p>
-                Dans la recherche des meilleurs candidats par notre connaissance des acteurs, le
+                les pratiques économiques du marché</p>
+             </div>
+            <div>
+                <h4>NOUS ACCOMPAGNONS</h4>
+                <p>Dans la recherche des meilleurs candidats par notre connaissance des acteurs, le
                 bouche à oreille, des interventions auprès de colloques et formations de
-                renoms…
-            </p>
-        </div>
-        <div>
-            <h4>NOUS VALORISONS</h4>
-            <p>
-                Sjob.com regroupant sous la forme d’un job-board les offres d’emploi de nos
-                clients, et proposant des outils pour favoriser leur attractivité.  
-            </p>
-        </div>
+                renoms…</p>
+            </div>
+            <div>
+                <h4>NOUS VALORISONS</h4>
+                <p>Sjob.com regroupant sous la forme d’un job-board les offres d’emploi de nos
+                clients, et proposant des outils pour favoriser leur attractivité.</p>
+            </div>
+        </section>    
     </div>
+
+    <footer>
+        
+        <nav>
+            <ul>
+                <li><a href="https://developer.mozilla.org/en-US/docs/Web/CSS/Specificity">Accueil</a></li>
+                <li><a href="https://developer.mozilla.org/en-US/docs/Web/CSS/Specificity">A propos</a></li>
+                <li><a href="https://developer.mozilla.org/en-US/docs/Web/CSS/Specificity">Parcourir les offres</a></li>
+                <li><a href="https://developer.mozilla.org/en-US/docs/Web/CSS/Specificity">Publier une offre</a></li>
+            </ul>
+        </nav>
+
+  
+
+        <div>
+            <img src="./logoEtIcon/facebook.png" width="100px" height="100px" alt="">
+            <img src="./logoEtIcon/logo-twitter.png" width="100px" height="100px" alt="">
+            <img src="./logoEtIcon/linkedin.png" width="100px" height="100px" alt="">
+        </div>
+
+        <div>
+            <img src="./logoEtIcon/logo.png">
+            <img src="./logoEtIcon/1200px-Copyright.svg.png" width="30px" height="30px">
+            <p>S’Job @2022. Tous droits réservés.</p>
+        </div>  
+
+
+    </footer>
 
 </body>
 </html>
