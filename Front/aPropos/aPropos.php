41600726640e7a31565fdb00584d3e6cbb5e6fd4
Alain <alainczr22@gmail.com>
2022-03-31 11:27:51 +0200

    codage html css de aPropos.php

---
+++ Front/aPropos/aPropos.php
@@ -0,0 +1,135 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="../Style/aPropos.css">
+    <link rel="stylesheet" href="../font/fontawesome-free-6.0.0-web/css/all.css">
+</head>
+<body>
+    <header>  
+        <div class="logo">
+                <img src="../logoEtIcon/logo.png" width="100px" height="100px">
+        </div>
+
+        <nav>
+            <ul>
+                <div id="allNav">
+                    <div class="flexNav">
+                        <li><a href="https://developer.mozilla.org/en-US/docs/Web/CSS/Specificity">Accueil</a></li>
+                        <li><a href="https://developer.mozilla.org/en-US/docs/Web/CSS/Specificity">A propos</a></li>
+                    </div>
+                    <div class="flexNav">
+                        <li><a href="https://developer.mozilla.org/en-US/docs/Web/CSS/Specificity">Parcourir les offres</a></li>
+                        <li><a href="https://developer.mozilla.org/en-US/docs/Web/CSS/Specificity">Publier une offre</a></li>
+                    </div>
+                </div>
+            </ul>
+        </nav>
+    </header>
+
+    <h1>Qui nous sommes ?</h1>
+
+    <section id="Alain">
+
+        <div class="profile"> 
+            <img src="../Image/Alainghibaudo.png" alt="">
+        
+            <p>ALAIN GHIBAUDO <br> Fondateur – CEO </p>
+        </div>
+
+        <div class="bioAlain">
+
+            <p>Bienvenue sur Sportcarriere, dans « votre » univers : le Sport et l’Entertainment ! 
+            Premier Sjob, Cabinet de recrutement et de conseil à s’être créé en Europe en 2001, nous sommes très fiers d’être les précurseurs du marché et de représenter l’expertise française. 
+            Acteurs nationaux ou internationaux, acteurs de proximité au cœur des problématiques sportives, Sjob vous propose un service global ressources humaines pour répondre à vos besoins. Tout un monde, un univers autour d’une passion commune : le sport. Chez Sjob, notre ambition est simple : offrir la meilleure expérience de recrutement possible pour les candidats et les entreprises du marché du sport et de l’Entertainment. 
+            Depuis 2001, nous développons des outils évolutifs adaptés aux spécificités et évolutions du secteur à travers notre plateforme Sjob.com et notre cabinet de recrutement expert du Sport et de l’Entertainment. 
+            Merci de votre fidélité, merci aux nouveaux venus dans notre communauté d’experts. Suivez-nous, ce n’est pas fini…</p>
+
+        </div>
+
+        <div class="signature">
+            <img src="../Image/signature.png" alt="">
+        </div>
+
+    </section>
+
+    <section>
+        <h4>POURQUOI NOUS CHOISIR ?</h4>
+        <div>
+            <div>
+                <img src="../logoEtIcon/valeurSC1.png" alt="">
+                <p>
+                    Nous sommes des créateurs de possibles 
+                   Nous croyons en la valeur de chaque individu et avons la 
+                   conviction que certaines rencontres peuvent changer le cours de notre vie. 
+                   C’est pourquoi nous mettons tout en œuvre pour créer 
+                   les meilleures conditions de rencontre entre les talents que 
+                   nous identifions et les entreprises qui nous confient la 
+                   constitution de leurs équipes. 
+                </p>
+            </div>
+            <div>
+                <img src="./logoEtIcon/valeurSC2.png" alt="">
+                <p></p>
+            </div>
+            <div>                
+                <img src="./logoEtIcon/valeurSC3.png" alt="">
+                <p></p>
+            </div>
+        </div>
+    </section>
+
+
+
+
+
+
+
+
+
+    
+    <footer>
+        <div id="allFooter">    
+            <nav>
+                
+                <ul>
+                    
+                    <li><a href="https://developer.mozilla.org/en-US/docs/Web/CSS/Specificity">Accueil</a></li>
+                    
+                    <li><a href="https://developer.mozilla.org/en-US/docs/Web/CSS/Specificity">A propos</a></li>
+                    
+                    <li><a href="https://developer.mozilla.org/en-US/docs/Web/CSS/Specificity">Parcourir les offres</a></li>
+                    
+                    <li><a href="https://developer.mozilla.org/en-US/docs/Web/CSS/Specificity">Publier une offre</a></li>
+                
+                </ul>
+            
+            </nav>
+
+            <div id="network">
+                
+                <img src="../logoEtIcon/facebook.png" width="100px" height="100px" alt="">
+                
+                <img src="../logoEtIcon/logo-twitter.png" width="100px" height="100px" alt="">
+                
+                <img src="../logoEtIcon/linkedin.png" width="100px" height="100px" alt="">
+            
+            </div>
+
+            
+            <div id="copyright">
+                <img src="../logoEtIcon/logo.png" width="353px" height="253px">
+                <div class="copy">
+                    <img src="../logoEtIcon/1200px-Copyright.svg.png" width="30px" height="30px">
+                
+                    <p>S’Job @2022. Tous droits réservés.</p>
+                </div>
+            </div>  
+        </div>
+    </footer>
+    
+</body>
+</html>
